84f49228fe038ed35b9311d537214a33c68750f1
plaetzaw <plaetzaw@gmail.com>
2021-01-19 14:17:02 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import './index.css'
 
 
 function App() {
   return (
+    <Router>
+      <Navbar/>
     <div className="App">
       Hello Whirl
     </div>
+    </Router>
   );
 }
 
