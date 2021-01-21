16c47a33768d31af435d268237ced38bac2bf72e
plaetzaw <plaetzaw@gmail.com>
2021-01-21 16:27:48 -0600

    Why wasn't this added?

--- src/components/Cards/FullMovieCard.js
+++ src/components/Cards/FullMovieCard.js
@@ -9,7 +9,6 @@ const FullMovieCard = () => {
         id: id
     }
     const { data: movie, error, isPending } = useFetch('http://localhost:8080/fullMovieInfo/', idObj);
-    // const { data } = axios.post('http://localhost:8080/fullMovieInfo/', idObj);
     console.log(movie)
     const history = useHistory();
   return (
