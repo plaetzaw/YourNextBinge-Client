a52c732e40f09ccd22339a7f2d3783012247eb38
plaetzaw <plaetzaw@gmail.com>
2021-01-26 13:19:50 -0600

    Added person links

--- src/components/Cards/FullPersonCard.js
+++ src/components/Cards/FullPersonCard.js
@@ -1,15 +1,19 @@
 import { Card, CardMedia, Paper } from '@material-ui/core'
+import {Link} from 'react-router-dom'
 import Placeholder from '../../utility/200x340.png'
 import './FullCard.css'
 import 'primeflex/primeflex.css';
 
 const FullPersonCard = ({props}) => {
   const info = props.data.info;
-  const credits = props.data.credits;
+  const moviecredits = props.data.moviecredits;
+  const tvcredits = props.data.tvcredits;
 
-  const creditsRender = credits.cast.map((credits) => {
+  console.log(info, moviecredits, tvcredits)
+
+  const moviecreditRender = moviecredits.cast.map((credits) => {
     const phChecker = credits.poster_path === null ?   (Placeholder) : (`https://image.tmdb.org/t/p/original/${credits.poster_path}`)
-    return <Card
+    return <Link to={`/movie/${credits.id}`}><Card
     key={credits.id}
     >
       <img
@@ -22,6 +26,25 @@ const FullPersonCard = ({props}) => {
       <br/>
       <i>Released: {credits.release_date}</i>
     </Card>
+    </Link>
+  })
+
+  const tvcreditRender = tvcredits.cast.map((credits) => {
+    const phChecker = credits.poster_path === null ?   (Placeholder) : (`https://image.tmdb.org/t/p/original/${credits.poster_path}`)
+    return <Link to={`/tvshow/${credits.id}`}><Card
+    key={credits.id}
+    >
+      <img
+      alt={credits.name}
+      src={phChecker}
+      height="400"
+      />
+      <li><i>in {credits.name}</i> as <br/>
+      <b>{credits.character}</b></li>
+      <br/>
+      <i>Premiered: {credits.first_air_date}</i>
+    </Card>
+    </Link>
   })
 
   return (
@@ -37,10 +60,15 @@ const FullPersonCard = ({props}) => {
         <br/>
         <div> <h3>{info.biography}</h3></div>
 
-        <h1>Credits for {info.name}</h1>
+        <h1>Movie Credits for {info.name}</h1>
         <div className="p-grid">
-            {creditsRender}
-            </div>    
+            {moviecreditRender}
+            </div>   
+
+             <h1>TV Credits for {info.name}</h1>
+        <div className="p-grid">
+            {tvcreditRender}
+            </div>  
     </Card>
     </>
   )
