30bff95833f6c2784617e8ccb5cc08132756950a
plaetzaw <plaetzaw@gmail.com>
2021-01-28 16:50:57 -0600

    Styled landing page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,28 @@
 import React from 'react'
-
+import TMDB from '../../utility/TMDB.png'
 const Home = () => {
   return (
     <>
+      <h1 className="loading">Welcome to YourNextBinge!</h1>
+      <br/>
       <div className="home">
-        <h1>Your Next Binge</h1><i className="fab fa-medapps"></i>
-        <p>Allows you to locate the your next bingewatching title</p>
-        <p>It also allows a user to look up a particular actor/actress
-          and the app will display all of their previous works</p>
-          <p>You can also search across movies and tv shows</p>
-          <span>Please note, the endpoints for the streaming are not provided by the API, but
-            it does show you where the title is available</span>
+        <div className="home-box">
+        <p>Allows you to locate the your next bingewatching title-either movie or tv show.</p>
+        <p>It also allows a user to look up a particular actor/actress.</p>
+        <p>YourNextBinge will display all of their previous performances.</p>
+        </div>
+
+        <div className="home-box">
+        <p>You can also search for a particular movie, tv-show or person.</p>
+        <p>Please note, the endpoints for the streaming are not provided by the API.</p>
+        <p> But it does show you where the title is available.</p>
+        </div>
+        <div className="home-box">
+        <p>Powered by</p>
+        <img src={TMDB}
+        className="home-tmdb"
+        />
+        </div>
       </div>
     </>
   )
