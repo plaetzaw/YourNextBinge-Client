520bd275927048763cb6f82b31e09806bc7e3af3
plaetzaw <plaetzaw@gmail.com>
2021-02-03 20:02:51 -0600

    Fixed 404 error clientside

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Redirect} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <>
       <h1>Page Not Found</h1>
       <p>The page you are searching for has not been found.</p>
-      <Redirect to="/"><span>Click here to return home</span></Redirect>
+      <span><Link to="/">Click here</Link> to return home</span>
     </>
   )
 }
